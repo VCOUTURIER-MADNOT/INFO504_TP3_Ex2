ac8be141ec15dab90ec6570d45e3dce9651b691d
coutcout <valcou@aliceadsl.fr>
2013-11-27 23:17:55 +0100

    Fin exo

--- WpfApplication3/MainWindow.xaml.cs
+++ WpfApplication3/MainWindow.xaml.cs
@@ -15,14 +15,68 @@ using System.Windows.Shapes;
 
 namespace WpfApplication3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            RowDefinition rd1 = new RowDefinition();
+            RowDefinition rd2 = new RowDefinition();
+
+            this.grid.RowDefinitions.Add(rd1);
+            this.grid.RowDefinitions.Add(rd2);
+            this.grid.ShowGridLines = true;
+
+            TextBox tb = new TextBox();
+            tb.Height = 200;
+            this.RegisterName("textBox", tb);
+            Grid.SetRow(tb, 0);
+
+            WrapPanel wp = new WrapPanel();
+            this.RegisterName("keyboard", wp);
+            Grid.SetRow(wp, 1);
+
+            this.grid.Children.Add(tb);
+            this.grid.Children.Add(wp);
+
+            generateKeyboard();
+
+            this.Height = this.grid.Height;
+        }
+
+        private void generateKeyboard()
+        {
+            WrapPanel wp = (WrapPanel) this.FindName("keyboard");
+            for(int i= 97; i <= 122; i++)
+            {
+                Button button = new Button();
+                button.Content = Convert.ToChar(i);
+                button.Width = 60;
+                button.Height = 50;
+                button.Click += onClick;
+                wp.Children.Add(button);
+            }
+        }
+
+        private void showKeyboard(Object sender, EventArgs e)
+        {
+            WrapPanel wp = (WrapPanel)this.FindName("keyboard");
+            wp.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void hideKeyboard(Object sender, EventArgs e)
+        {
+            WrapPanel wp = (WrapPanel)this.FindName("keyboard");
+            wp.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void onClick(Object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            TextBox tb = (TextBox)this.FindName("textBox");
+
+            tb.Text += button.Content;
         }
     }
 }
